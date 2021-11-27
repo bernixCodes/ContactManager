b6853c353b3f6acfd67553ebc86199412bae6c68
bernice <bernice@hubtel.com>
2021-11-27 01:30:03 +0000

    alerts and redirect

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,9 +1,13 @@
 import React,{useState} from 'react'
+import { useNavigate  } from 'react-router-dom';
 
 function AddContact(props) {
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
+    const [successAlert, setSuccessAlert] = useState('');
+    const [dangerAlert, setDangerAlert] = useState('');
+    const navigate = useNavigate();
 
     const handleName = (e)=>{
         setName(e.target.value)
@@ -19,7 +23,7 @@ function AddContact(props) {
     const addContact = (e)=>{
         e.preventDefault();
         if(!name || !phone || !email){
-            alert('Heyyy!! all fields are required')
+            setDangerAlert('All fields are required')
             return
         }
         let payload ={
@@ -27,16 +31,47 @@ function AddContact(props) {
             phone: phone,
             email:email
         }
+         setSuccessAlert('Successfully added a contact')
          props.newContactDetail(payload);
          setName('')
          setPhone('')
          setEmail('')
+         setTimeout(()=>{
+             navigate('/')
+         }, 3000)
+
+         ;
        
     }
 
 
     return (
         <div className='container'>
+            {
+               dangerAlert&&(
+                <div className="alert alert-danger alert-dismissible fade show " role="alert">
+                <span >{dangerAlert}
+                <span type="button" className="close float-end" data-bs-dismiss="alert"  aria-label="Close"aria-hidden="true">&times;</span>
+                
+                </span>
+              </div>
+               )
+
+            }
+
+            {
+                successAlert &&(
+                    <div className="alert alert-success alert-dismissible fade show " role="alert">
+                       <span> {successAlert}
+                       <span type="button" className="close float-end" data-bs-dismiss="alert" aria-label="Close"aria-hidden="true">&times;</span>
+
+                       </span>
+                   </div>
+                )
+            }
+           
+
+                 
             <h2 className='mt-3 mb-2'>Add Contact</h2>
               <form onSubmit={addContact}>
                 <div className="mb-3">
@@ -51,6 +86,7 @@ function AddContact(props) {
                     <label  className="form-label">Email Address</label>
                     <input type="email" value={email} className="form-control" id="email" onChange={handleEmail} />
                 </div>
+                
                 <button type="submit" className="btn btn-primary">Add</button>
               </form>
         </div>
